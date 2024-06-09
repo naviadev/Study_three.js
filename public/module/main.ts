e6e86ca4ea6bcc3d6811bc8b8c761c57c4b51be8
naviadev <qo2oq1020@gmail.com>
2024-06-09 21:13:04 +0900

    main.ts -> MESH를 통해 3D 정육면체 생성, Renderer를 통해 생성된 정육면체를 렌더링 하여 출력 + LINE 그리기

---
+++ public/module/main.ts
@@ -0,0 +1,41 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
+
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+
+// line
+
+const lineMaterial = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+const lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
+
+const line = new THREE.Line( lineGeometry, lineMaterial );
+
+scene.add( line );
+scene.add(cube);
+
+// camera.position.z = 5;
+
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
